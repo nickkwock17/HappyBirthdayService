5600071220f7ec288613fa1ae9ddf42ae84480e4
Havish <Havish747@gmail.com>
2026-04-24 19:45:48 -0700

    Use LA timezone instead of UTC

--- lambda/email_generator.py
+++ lambda/email_generator.py
@@ -1,13 +1,15 @@
-from datetime import date
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from typing import List
 from birthday_schema import BirthdaySchema, Email
 
 def generate_emails(birthdays: List[BirthdaySchema]) -> List[Email]:
-    today = date.today()
+    today = datetime.now(ZoneInfo("America/Los_Angeles")).date()
     emails = []
 
     for birthday in birthdays:
-        if (birthday.birth_date.month == today.month and birthday.birth_date.day == today.day) or birthday.is_integration_test:            emails.append(Email(
+        if (birthday.birth_date.month == today.month and birthday.birth_date.day == today.day) or birthday.is_integration_test:
+            emails.append(Email(
                 to=birthday.email,
                 subject=f"Happy Birthday, {birthday.name}!",
                 body=(
